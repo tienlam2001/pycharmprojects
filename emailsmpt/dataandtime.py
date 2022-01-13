13c1b440f070bb448648ffe95fff2edefd02c61d
tienlam2001 <tienlam2001@gmail.com>
2022-01-13 14:45:15 -0500

    File Motivation created to send motivation through email

--- emailsmpt/dataandtime.py
+++ emailsmpt/dataandtime.py
@@ -1,3 +1,12 @@
 import datatime as dt
 
-now = dt.datatime.now
+now = dt.datatime.now()
+print(now)
+year = now.year()
+print(year)
+month = now.month()
+data_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datatime(year=1995, month=12, day =15)
+print(date_of_birth)
--- emailsmpt/main.py
+++ emailsmpt/main.py
@@ -1,1 +1,2 @@
-import dateandtime
+import motivationQuote
+

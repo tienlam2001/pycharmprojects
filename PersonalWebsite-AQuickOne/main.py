1fdb558600978bb09098dfcc62932da90596ac60
Lam Van <lamv@Lams-iMac.local>
2022-05-27 00:04:44 -0400

    this

--- PersonalWebsite-AQuickOne/main.py
+++ PersonalWebsite-AQuickOne/main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+# This is a sample Python script.
 
-app =  Flask(__name__)
+# Press ⌘R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True)
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

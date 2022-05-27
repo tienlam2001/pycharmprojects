a61cae37304abeea7bd1bf63cd3c6aba133df826
Lam Van <tienlam2001@gmail.com>
2022-05-27 00:03:01 -0400

    sdfsd

--- PersonalWebsite-AQuickOne/main.py
+++ PersonalWebsite-AQuickOne/main.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app =  Flask(__name__)
 
+images = ["html.png","csslogo.png","reactjs.png","javascript.png","phplogo.png","pythonIcon.png","javaIcon.png"]
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def renderAbout():
+    return render_template("about.html")
+
+@app.route("/skill")
+def renderSkill():
+    return render_template("skillPage.html", images=images)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- PersonalWebsite-AQuickOne/static/js/index.js
+++ PersonalWebsite-AQuickOne/static/js/index.js
@@ -6,6 +6,7 @@ function setup() {
 //    frameRate(30)
     for(let lcv = 0 ; lcv < 40; lcv++){
             var randomX = random(0,1500)
+            console.log(randomX)
             var randomY = random(0,900)
             array.push(new Particles(randomX,randomY))
 
@@ -18,14 +19,15 @@ function draw() {
     background(0)
     for(let lcv = 0; lcv < array.length; lcv++){
         array[lcv].appear();
+        array[lcv].move();
     }
 
 
 }
 
 function Particles(randomX,randomY){
-    this.x = 450;
-    this.y = 450;
+    this.x = randomX;
+    this.y = randomY;
     this.vx = 0.1
     this.vy = 0;
 
